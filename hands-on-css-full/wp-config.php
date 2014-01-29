39b356869e2da63876839c9f307b7e427dc48efa
root <root@wordpress.(none)>
2014-01-29 04:52:13 +0000

    updated WP core, EDD and SEO plugins

--- hands-on-css-full/wp-config.php
+++ hands-on-css-full/wp-config.php
@@ -1,19 +1,19 @@
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', '3585624991bdf29752055450e8ff922c');
+define('DB_PASSWORD', '3ee1504a19541e842c31ca621a8f5a7d');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('AUTH_KEY', '85c6c99f02aca5ea6d7f12c3f03cf1e29d3cc8baeb168583775edeac1cc03ec8');
-define('AUTH_SALT', 'ec3aaf73c15bedca4c617e74df56197798a71b8556924c853351a0d153294b49');
-define('AUTH_KEY', '85c6c99f02aca5ea6d7f12c3f03cf1e29d3cc8baeb168583775edeac1cc03ec8');
-define('AUTH_SALT', 'ec3aaf73c15bedca4c617e74df56197798a71b8556924c853351a0d153294b49');
-define('LOGGED_IN_KEY', '0d0f904bdd08794170596b46b42dc89d530c779ac036eb240cabe1c0fc164d07');
-define('LOGGED_IN_SALT', '38305a37343d814f323b0a4a005e2bac9ff8abcebe055032fbb3cacb98339661');
-define('NONCE_KEY', '0e265621f4c4613edf02187636889e5cc34c66316b8cfe480a32d7314ca85265');
-define('NONCE_SALT', '5186d3f44cb0a895e83012f1dd510879916f85d62726a4207eb7361c519b62bf');
+define('AUTH_KEY', '8ad6d16c515bc58f913b71f9ede2d83c969cb00aaa3d4f3fe07125427bc15545');
+define('AUTH_SALT', '16f92af3055aa3c7928e59bba925183f1dbc76350a0f2f2bc901b6f4f00d2d54');
+define('AUTH_KEY', '8ad6d16c515bc58f913b71f9ede2d83c969cb00aaa3d4f3fe07125427bc15545');
+define('AUTH_SALT', '16f92af3055aa3c7928e59bba925183f1dbc76350a0f2f2bc901b6f4f00d2d54');
+define('LOGGED_IN_KEY', 'a2ecd468b11671dfee7bd921fbc617eb6267a54c8b023bcc622a7cf99078ffcd');
+define('LOGGED_IN_SALT', 'bfa875a48cb1ffc2717c071e38111ce93cff7c04743b20312157740f53f94e11');
+define('NONCE_KEY', 'ac11c439f5e53f51ef4077af1204c1ca6f2b8ef70d56c980da62adea50bc858b');
+define('NONCE_SALT', '4c48374865ca20c4b294b881ed1381b9f6ede6d816aa1993282a6edae6bb9498');
 
 $table_prefix  = 'handson20131111_';
 
